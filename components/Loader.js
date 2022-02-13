9849db9c40078b82dc60fa4369ba52db3481ab62
Joël Mik <joel@megosu.com>
2022-02-13 23:04:03 +0100

    added button

--- components/Loader.js
+++ components/Loader.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import {Html, useProgress} from "@react-three/drei";
+import {Html} from "@react-three/drei";
 const Loader = () => {
-  // const {progress, loaded} = useProgress();
   return (
     <>
     <Html center><div className="h-5 w-5 animate-spin">-</div></Html>
     </>
-    
   )
 }
 
