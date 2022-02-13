d26b823d5a0abea0a6c63f86238e36af99a1a81c
Joël Mik <joel@megosu.com>
2022-02-13 17:57:39 +0100

    margin removed

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -11,7 +11,7 @@ function LandingPage() {
     <div className="px-5 lg:px-20 h-[90vh] font-poppins flex items-center relative text-slate-300">
       <div className="z-10 relative w-full pointer-events-none">
       <h1 className="text-6xl md:text-[180px] font-bebas">Posterity Watches</h1>
-      <h3 className="text-3xl font-poppin font-light -mt-10">A Metaverse Luxury Brand</h3>
+      <h3 className="text-3xl font-poppins font-light">A Metaverse Luxury Brand</h3>
       </div>
       <div className="absolute h-5/6 w-1/2 right-0">
         <Canvas>
