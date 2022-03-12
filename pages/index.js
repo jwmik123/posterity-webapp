f2f9b0b3e97bab90fae0eccdc3a85e2889797559
Joël Mik <joel@megosu.com>
2022-03-12 11:58:16 +0100

    small changes

--- pages/index.js
+++ pages/index.js
@@ -23,19 +23,19 @@ export default function Home() {
         <Roadmap id="roadmap" />
         <Creators />
       </main>
-      <footer className="bg-slate-500 flex flex-col md:flex-row gap-5 text-center md:text-left justify-between items-center px-5 md:px-20 lg:px-44 py-5">
-        <div className={ "text-slate-900 font-bebas text-5xl py-2 px-4 rounded h-18"}>Posterity.</div>
+      <footer className="bg-black flex flex-col md:flex-row gap-5 text-center md:text-left justify-between items-center px-5 md:px-20 lg:px-44 py-5">
+        <div className={ "text-slate-100 font-bebas text-5xl py-2 px-4 rounded h-18"}>Posterity.</div>
         <div>
-          <p className="text-xl">&copy;2022 POSTERITY.</p>
+          <p className="text-xl text-slate-100">&copy;2022 POSTERITY.</p>
         </div>
-        <div className="text-lg flex flex-col">
+        <div className="text-lg text-slate-100 flex flex-col">
           <a href='#'>Terms of Service</a>
           <a href='#'>Privacy Policy</a>
         </div>
         <div className="flex flex-row gap-4">
-          <button className="bg-slate-900 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><FaTwitter className="w-6 h-6"/></button>
-          <button className="bg-slate-900 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><FaDiscord className="w-6 h-6"/></button>
-          <button className="bg-slate-900 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><AiFillInstagram className="w-6 h-6"/></button>
+          <button className="bg-slate-700 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><FaTwitter className="w-6 h-6"/></button>
+          <button className="bg-slate-700 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><FaDiscord className="w-6 h-6"/></button>
+          <button className="bg-slate-700 text-slate-100 p-2 rounded-xl hover:scale-110 duration-100"><AiFillInstagram className="w-6 h-6"/></button>
         </div>
       </footer>
       </>
