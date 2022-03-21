efd3848fababb9b72139a57a5a960f1ca6e97400
Joël Mik <joel@megosu.com>
2022-03-21 18:02:06 +0100

    mulitple changes

--- components/FaqPage.js
+++ components/FaqPage.js
@@ -5,17 +5,18 @@ import { BsFillCaretDownFill } from "react-icons/bs";
 const FaqPage = () => {
     const data = {
         rows: [
-            {
-                title: <h1 className="text-4xl text-slate-100 font-bebas">What is an NFT?</h1>,
-                content: <p className="text-lg text-slate-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vel excepturi inventore facere commodi possimus, repellendus itaque, corporis similique consequuntur delectus aliquid illum illo nulla debitis hic repellat magnam. Aperiam.</p>,
-            },
+            
             {
                 title: <h1 className="text-4xl text-slate-100 font-bebas">How can I buy a Posterity Watch?</h1>,
-                content: <p className="text-lg text-slate-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vel excepturi inventore facere commodi possimus, repellendus itaque, corporis similique consequuntur delectus aliquid illum illo nulla debitis hic repellat magnam. Aperiam.</p>,
+                content: <p className="text-lg text-slate-100">You will be able to mint a Posterity Watch through the link in our Discord or clicking on the Mint button on this page.</p>,
+            },
+            {
+                title: <h1 className="text-4xl text-slate-100 font-bebas">Will there be a whitelist and how do I get on?</h1>,
+                content: <p className="text-lg text-slate-100">Yes there will be a whitelist of 1500 members. You can get access to the whitelist through the discord and following the steps over there.</p>,
             },
             {
                 title: <h1 className="text-4xl text-slate-100 font-bebas">What will the mint price be?</h1>,
-                content: <p className="text-lg text-slate-100">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos vel excepturi inventore facere commodi possimus, repellendus itaque, corporis similique consequuntur delectus aliquid illum illo nulla debitis hic repellat magnam. Aperiam.</p>,
+                content: <p className="text-lg text-slate-100">The regular mint price will be set at 0.3 ETH. Mint price for whitelist will be 0.25 ETH.</p>,
             },
             {
                 title: <h1 className="text-4xl text-slate-100 font-bebas">Will the Posterity Watch be listed on exchanges?</h1>,
