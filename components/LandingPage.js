bc424b09aa1c202e4610e04d53f84c73e0ff15e5
Joël Mik <joel@megosu.com>
2022-03-29 21:17:27 +0200

    footer

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -39,7 +39,7 @@ function LandingPage() {
           )
         }
       </div>
-      <div className="bg-red-900 mt-36 w-full">
+      <div className="bg-red-900 mt-36 w-full lg:hidden">
         <h1 className="text-6xl font-staat text-center">Posterity Watches</h1>
       </div>
     </div>
