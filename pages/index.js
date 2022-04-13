c3b7edcd28b8e4cf179e815f32d37a5436e60f03
Joël Mik <joel@megosu.com>
2022-04-13 16:08:54 +0200

    mailinglist removed

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <LandingPage />
         <Banner />
         <Roadmap id="roadmap" />
-        <MailingBanner />
+        {/* <MailingBanner /> */}
         <Creators />
         <Faq />
       </main>
