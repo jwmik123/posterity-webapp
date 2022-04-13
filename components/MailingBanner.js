f9d946c30f7171d329e98552230672cfbade80e5
Joël Mik <joel@megosu.com>
2022-04-13 23:41:08 +0200

    subscribe banner is responsive

--- components/MailingBanner.js
+++ components/MailingBanner.js
@@ -1,10 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
-import dynamic from 'next/dynamic';
 import Image from 'next/image';
-
-// Als je iets wilt importeren dat client side moet werken, moet je op deze manier importen met NextJS.
-const isMobile = dynamic(() => import('react-device-detect'));
 
 import subscribeImage from '../assets/topview.png';
 
@@ -24,28 +20,26 @@ const CustomForm = ({ status, message, onValidated }) => {
     })
 
   return (
-    <div className="w-1/2 flex flex-col justify-center font-roboto py-24">
+    <div className="w-full md:w-1/2 flex flex-col justify-center font-roboto py-24">
       <h1 className="text-6xl font-staat text-ketic text-center pb-10" data-aos="fade-up" data-aos-delay="50">Stay up to date.</h1>
       <div className="w-full flex justify-center">
         <div className="flex justify-between rounded items-center p-2 border-2 bg-gray w-3/4" data-aos="fade-up">
-        <input
-          className="bg-gray text-ketic pl-3 outline-none"
-          type="email" 
-          defaultValue={email}
-          onChange={e => setEmail(e.target.value)}
-          placeholder="johndoe@email.com"
-          required
-        />
-        <button className="bg-ketic rounded text-gray px-5 h-14" onClick={submit}>
-          {status === "sending" ? (
-            <span>Sending...</span>
-          ) : <span>Subscribe</span>}
-        </button>
+          <input
+            className="bg-gray text-ketic pl-3 outline-none"
+            type="email" 
+            defaultValue={email}
+            onChange={e => setEmail(e.target.value)}
+            placeholder="johndoe@email.com"
+            required
+          />
+          <button className="bg-ketic rounded text-gray px-5 h-14" onClick={submit}>
+            {status === "sending" ? (
+              <span>Sending...</span>
+            ) : <span>Subscribe</span>}
+          </button>
+        </div>
       </div>
-      </div>
-      
-      
-     
+
       {status === "error" && (
         <div className="text-red pt-4" dangerouslySetInnerHTML={{__html: message}} />
       )}
@@ -60,8 +54,8 @@ const MailingBanner = () => {
   const postUrl = `https://posteritywatches.us14.list-manage.com/subscribe/post?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U}&id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID}`;
 
   return (
-    <div className={"mailchimp bg-sky flex"}>
-      <div className="w-1/2 relative">
+    <div className="mailchimp bg-sky flex flex-col md:flex-row my-10">
+      <div className="w-full h-64 md:h-auto md:w-1/2 relative">
         <Image 
           src={subscribeImage}
           alt="posterity-subscribe-watch"
