db1357d3a78a9606e593640b4aeba6ce0843751f
Joël Mik <joel@megosu.com>
2022-04-26 15:10:56 +0200

    typo's

--- components/Creators.js
+++ components/Creators.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
 import watch from "../public/images/posterity_3.png";
 import watch1 from "../public/images/posterity_4.png";
@@ -9,125 +9,256 @@ import watch4 from "../public/images/posterity_7.png";
 import watch5 from "../public/images/posterity_10.png";
 import watch6 from "../public/images/posterity.png";
 
-
 const Creators = () => {
   return (
     <div className="py-20 text-slate-100 flex flex-col items-center">
-      <h1 className="text-7xl font-staat text-center mb-10" data-aos="fade-up">The team.</h1>
+      <h1 className="text-7xl font-staat text-center mb-10" data-aos="fade-up">
+        The team.
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 w-3/4">
-  
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Tyler.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">Co-founder <br /> Community Lead</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Tyler.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                Co-founder <br /> Community Lead
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-                Also known as the guy who loves to help and bring people together. But don&apos;t mistake his kindness for weakness, he will ban you if you act like a shitbag!
-              </p>     
-              
+                Also known as the guy who loves to help and bring people
+                together. But don&apos;t mistake his kindness for weakness, he
+                will ban you if you act like a shitbag!
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75  duration-150" layout="fill" objectFit="cover" priority src={watch} quality={100} alt="tyler"/>
+            <Image
+              className="rounded-3xl opacity-75  duration-150"
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch}
+              quality={100}
+              alt="tyler"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="50">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="50"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Mars.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">Co-founder <br/> Marketing Lead</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Mars.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                Co-founder <br /> Marketing Lead
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-              A marketing and psychology enthusiast, who tries to understand people to the point where computers are his preferred company.
-              </p>     
-              
+                A marketing and psychology enthusiast, who tries to understand
+                people to the point where computers are his preferred company.
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75 " layout="fill" objectFit="cover" priority src={watch4} quality={100} alt="marc" />
+            <Image
+              className="rounded-3xl opacity-75 "
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch4}
+              quality={100}
+              alt="marc"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="100">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Jalla.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">Co-founder <br/> Blockchain Developer</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Jalla.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                Co-founder <br /> Blockchain Developer
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-              Incredibly handsome guy who can also code a little bit. Oh, one of the two statements mentioned is a lie.
-              </p>     
-
-
+                Incredibly handsome guy who can also code a little bit. Oh, one
+                of the two statements mentioned is a lie.
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75 " layout="fill" objectFit="cover" priority src={watch6} quality={100} alt="jalla" />
+            <Image
+              className="rounded-3xl opacity-75 "
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch6}
+              quality={100}
+              alt="jalla"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="50">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="50"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Wessa.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">Blockchain Developer</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Wessa.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                Blockchain Developer
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-                A programmer as long as he can remember. He only makes code in the dark because light attracts bugs, and he hate all types of bugs.
-              </p>     
-              
+                A programmer as long as he can remember. He only makes code in
+                the dark because light attracts bugs, and he hates all types of
+                bugs.
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75 " layout="fill" objectFit="cover" priority src={watch3} quality={100} alt="wessa" />
+            <Image
+              className="rounded-3xl opacity-75 "
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch3}
+              quality={100}
+              alt="wessa"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="100">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="100"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Kev.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">3D Design Lead</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Kev.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                3D Design Lead
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-              Ugliest of the group, but certainly not the dumbest (Jalla). Ps. I&apos;m buying a house in the metaverse soon. See you guys there! </p>          
+                Ugliest of the group, but certainly not the dumbest (Jalla). Ps.
+                I&apos;m buying a house in the metaverse soon. See you guys
+                there!
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75 " layout="fill" objectFit="cover" priority src={watch1} quality={100} alt="kev" />
+            <Image
+              className="rounded-3xl opacity-75 "
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch1}
+              quality={100}
+              alt="kev"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="150">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="150"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Luke.</h2>
-              <h5 className="font-roboto text-xl group-hover:hidden">3D Motion Design</h5>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Luke.
+              </h2>
+              <h5 className="font-roboto text-xl group-hover:hidden">
+                3D Motion Design
+              </h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-                A wild fella stranded in the motion designer world. Sounds like the beginning of a bad joke right? Just wait until you see my crypto investments.
-              </p>     
-              
+                A wild fella stranded in the motion designer world. Sounds like
+                the beginning of a bad joke right? Just wait until you see my
+                crypto investments.
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75" layout="fill" objectFit="cover" priority src={watch5} quality={100} alt="Luke" />
+            <Image
+              className="rounded-3xl opacity-75"
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch5}
+              quality={100}
+              alt="Luke"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="50">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="50"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Tom.</h2>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Tom.
+              </h2>
               <h5 className="font-roboto text-xl group-hover:hidden">Sales</h5>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
-                Tried to understand Solidity to become a full-time developer (Jalla is my idol), but ended up in mastering the art of selling. Not like Jordan Belfort, but with standards and values.
-              </p>     
+                Tried to understand Solidity to become a full-time developer
+                (Jalla is my idol), but ended up in mastering the art of
+                selling. Not like Jordan Belfort, but with standards and values.
+              </p>
             </div>
-            <Image className="rounded-3xl opacity-75 " layout="fill" objectFit="cover" priority src={watch} quality={100} alt="Tom" />
+            <Image
+              className="rounded-3xl opacity-75 "
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch}
+              quality={100}
+              alt="Tom"
+            />
           </div>
         </div>
 
-        <div className="creator flex justify-center flex-col items-center" data-aos="fade-up" data-aos-delay="350">
+        <div
+          className="creator flex justify-center flex-col items-center"
+          data-aos="fade-up"
+          data-aos-delay="350"
+        >
           <div className="aspect-square w-80 md:w-64 lg:w-80 flex items-center justify-center relative group hover:scale-105 transition-all duration-150">
             <div className="absolute z-10">
-              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">Join us.</h2>
+              <h2 className="font-staat text-6xl group-hover:text-3xl group-hover:text-left px-5">
+                Join us.
+              </h2>
               <p className="text-lg font-roboto font-light text-left px-5 hidden group-hover:block md:group-hover:text-sm lg:group-hover:text-lg">
                 Do you think you have what it takes?
-              </p> 
-              <button className="hidden mx-5 mt-2 group-hover:flex flex-row items-center gap-2 bg-sky p-2 rounded text-ketic"><a href="mailto:info@posteritywatches.io">Apply Now</a></button> 
+              </p>
+              <button className="hidden mx-5 mt-2 group-hover:flex flex-row items-center gap-2 bg-sky p-2 rounded text-ketic">
+                <a href="mailto:info@posteritywatches.io">Apply Now</a>
+              </button>
             </div>
-            <Image className="rounded-3xl opacity-75" layout="fill" objectFit="cover" priority src={watch2} quality={100} alt="joinus" />
+            <Image
+              className="rounded-3xl opacity-75"
+              layout="fill"
+              objectFit="cover"
+              priority
+              src={watch2}
+              quality={100}
+              alt="joinus"
+            />
           </div>
         </div>
-      </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Creators
+export default Creators;
--- components/MailingBanner.js
+++ components/MailingBanner.js
@@ -1,54 +1,76 @@
-import React, { useState, useEffect } from 'react';
-import MailchimpSubscribe from 'react-mailchimp-subscribe';
-import Image from 'next/image';
+import React, { useState, useEffect } from "react";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
+import Image from "next/image";
 
-import subscribeImage from '../assets/topview.png';
+import subscribeImage from "../assets/topview.png";
 
 const CustomForm = ({ status, message, onValidated }) => {
-  const [email, setEmail] = useState('');
-  const clearField = () => setEmail('');
+  const [email, setEmail] = useState("");
+  const clearField = () => {
+    setEmail("");
+  };
 
   useEffect(() => {
-    if(status === "success") clearField();
-  }, [status])
+    if (status === "success") clearField();
+  }, [status]);
 
   const submit = () =>
-    email && 
+    email &&
     email.toString().indexOf("@") > -1 &&
-    onValidated({ 
-      MERGE0: email 
-    })
+    onValidated({
+      MERGE0: email,
+    });
 
   return (
     <div className="w-full md:w-1/2 flex flex-col justify-center font-roboto py-24">
-      <h1 className="text-6xl font-staat text-ketic text-center pb-10" data-aos="fade-up" data-aos-delay="50">Stay up to date.</h1>
+      <h1
+        className="text-6xl font-staat text-ketic text-center pb-10"
+        data-aos="fade-up"
+        data-aos-delay="50"
+      >
+        Stay up to date.
+      </h1>
       <div className="w-full flex justify-center">
-        <div className="flex justify-between rounded items-center p-2 border-2 bg-gray w-3/4" data-aos="fade-up">
+        <div
+          className="flex justify-between rounded items-center p-2 border-2 bg-gray w-3/4"
+          data-aos="fade-up"
+        >
           <input
             className="bg-gray text-ketic pl-3 outline-none"
-            type="email" 
+            type="email"
             defaultValue={email}
-            onChange={e => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)}
             placeholder="johndoe@email.com"
             required
           />
-          <button className="bg-ketic rounded text-gray px-5 h-14" onClick={submit}>
+          <button
+            className="bg-ketic rounded text-gray px-5 h-14"
+            onClick={submit}
+          >
             {status === "sending" ? (
               <span>Sending...</span>
-            ) : <span>Subscribe</span>}
+            ) : (
+              <span>Subscribe</span>
+            )}
           </button>
         </div>
       </div>
 
       {status === "error" && (
-        <div className="text-red text-center pt-4" dangerouslySetInnerHTML={{__html: message}} />
+        <div
+          className="text-red text-center pt-4"
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
       )}
       {status === "success" && (
-        <div className="text-ketic text-center pt-2" dangerouslySetInnerHTML={{__html: message}} />
+        <div
+          className="text-ketic text-center pt-2"
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
       )}
     </div>
-  )
-}
+  );
+};
 
 const MailingBanner = () => {
   const postUrl = `https://posteritywatches.us14.list-manage.com/subscribe/post?u=${process.env.NEXT_PUBLIC_MAILCHIMP_U}&id=${process.env.NEXT_PUBLIC_MAILCHIMP_ID}`;
@@ -56,25 +78,25 @@ const MailingBanner = () => {
   return (
     <div className="mailchimp bg-sky flex flex-col md:flex-row my-10">
       <div className="w-full h-64 md:h-auto md:w-1/2 relative">
-        <Image 
+        <Image
           src={subscribeImage}
           alt="posterity-subscribe-watch"
           layout="fill"
           objectFit="cover"
         />
       </div>
-      <MailchimpSubscribe 
+      <MailchimpSubscribe
         url={postUrl}
         render={({ subscribe, status, message }) => (
           <CustomForm
             status={status}
             message={message}
-            onValidated={formData => subscribe(formData)}
-          /> 
+            onValidated={(formData) => subscribe(formData)}
+          />
         )}
       />
     </div>
-  )
-}
+  );
+};
 
 export default MailingBanner;
